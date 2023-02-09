136bb250bd2ad94067ad8ee3402376ca0d560d8d
FelKor <felix_korte@outlook.de>
2023-02-09 15:43:16 +0100

    first commit

---
+++ EAN-check/EANcheck.java
@@ -0,0 +1,133 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class EANcheck extends JFrame implements ActionListener{
+    
+    //EAN Code decoder
+    JTextField inputOutputEANcode;
+    JPanel EANchecker;
+
+    //check-nummer-berechnung
+    JTextField inputUncompleteEAN, outputCompleteEAN;
+    JPanel checknumbercalc;
+
+    //Gui
+    public EANcheck(){
+        setTitle("EAN Codierungs Job");
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setLayout(new BorderLayout());
+
+        //EAN Code checker
+        inputOutputEANcode = new JTextField(13);
+        inputOutputEANcode.addActionListener(this);
+        inputOutputEANcode.setActionCommand("input");
+
+        EANchecker = new JPanel();
+        EANchecker.add(new JLabel("Your EAN-Code:"));
+        EANchecker.add(inputOutputEANcode);
+
+        //check-nummer-berechner
+        inputUncompleteEAN = new JTextField(13);
+        inputUncompleteEAN.addActionListener(this);
+        inputUncompleteEAN.setActionCommand("inputUncomplete");
+        outputCompleteEAN = new JTextField(13);
+        outputCompleteEAN.setEditable(false);
+
+        checknumbercalc = new JPanel();
+        checknumbercalc.add(new JLabel("Your unfinished EAN-Code"));
+        checknumbercalc.add(inputUncompleteEAN);
+        checknumbercalc.add(new JLabel("Your EAN-Code with check-number:"));
+        checknumbercalc.add(outputCompleteEAN);
+
+        //Frame
+        add(EANchecker, BorderLayout.NORTH);
+        add(checknumbercalc, BorderLayout.CENTER);
+    }
+
+    private int sumWhithoutCheckNumber(String strin){
+        
+        int sumWhithoutCheckNumber = 0;
+        String str = strin;
+        int codeLength = str.length();
+        char[] EANnumbers = new char[codeLength];
+        
+        if(strin.equals(inputUncompleteEAN.getText()))
+            str += "0";
+
+        for(int i = 0; i < codeLength; i++ )
+            EANnumbers[codeLength-1-i] = str.charAt(i);
+
+        for(int i = 0; i<codeLength; i++){
+            if(i%2==0)
+                sumWhithoutCheckNumber += Character.getNumericValue(EANnumbers[i])*1;
+            else
+                sumWhithoutCheckNumber += Character.getNumericValue(EANnumbers[i])*3;
+        }
+        return sumWhithoutCheckNumber;
+    }
+
+    //checking the EAN code
+    public void EANcodecheck(){
+        int checkNumber;
+        boolean error = false;
+        String str = inputOutputEANcode.getText();
+
+        try{
+            Integer.parseInt(str);
+        }
+        catch(NumberFormatException ex){
+            inputOutputEANcode.setText("Nur Zahlen");
+            error = true;
+        }
+
+        if(error == false){
+            //checknumber
+            checkNumber = Character.getNumericValue(str.charAt(str.length()-1));
+        
+            //check if EAN-Code correct
+            if(((sumWhithoutCheckNumber(inputOutputEANcode.getText())-Character.getNumericValue(str.charAt(str.length()-1)))+checkNumber)%10==0)
+                inputOutputEANcode.setText("The Code is correct!");
+            else
+                inputOutputEANcode.setText("The Code is false!");
+        }
+    }
+
+    //calculating the check number
+    private void checkNumbercalculator(){
+        int checkNumber;
+        boolean error = false;
+
+        try{
+            Integer.parseInt(inputUncompleteEAN.getText());
+        }
+        catch(NumberFormatException ex){
+            inputUncompleteEAN.setText("Nur Zahlen");
+            error = true;
+        }
+        
+        if(error == false){
+            checkNumber = 10-(sumWhithoutCheckNumber(inputUncompleteEAN.getText())%10);
+    
+            outputCompleteEAN.setText(inputUncompleteEAN.getText() + checkNumber);
+            inputUncompleteEAN.setText("");
+        }
+    }
+
+    //control
+    public void actionPerformed(ActionEvent e){
+        
+        //EAN Code checker
+        if(e.getActionCommand().equals("input"))
+            EANcodecheck();
+
+        if(e.getActionCommand().equals("inputUncomplete"))
+            checkNumbercalculator();
+    }
+
+    public static void main(String[] args){
+        EANcheck App = new EANcheck();
+        App.setVisible(true);
+        App.setSize(500, 500);
+    }
+}
---
+++ Taschenrechner/Taschenrechner.java
@@ -0,0 +1,286 @@
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.Box;
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+public class Taschenrechner extends JFrame implements ActionListener {
+
+    double num1, num2, sum;
+    String sumString = "", operatorString = "", inputString1 = "", inputString2 = "";
+    boolean changeinputString = false, inputtingcurrentNumbers = true;
+
+    JButton number0Button, number1Button, number2Button, number3Button, number4Button,
+            number5Button,number6Button, number7Button, number8Button, number9Button;
+    JButton addButton, subtractButton, multiplyButton, divideButton, clearButton, equalsButton; //dotButton
+    
+    JPanel numpadPanel;
+
+    JTextField currentNumbersField, sumField;
+    Box outputBox, outputBoxleft, outputBoxright;
+
+    //Konstruktor
+    public Taschenrechner(){
+        setTitle("Taschenrechner");
+        setLayout(new BorderLayout());
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+        //number
+        number0Button = new JButton("0");
+        number0Button.addActionListener(this);
+        number0Button.setActionCommand("0");
+        number1Button = new JButton("1");
+        number1Button.addActionListener(this);
+        number1Button.setActionCommand("1");
+        number2Button = new JButton("2");
+        number2Button.addActionListener(this);
+        number2Button.setActionCommand("2");
+        number3Button = new JButton("3");
+        number3Button.addActionListener(this);
+        number3Button.setActionCommand("3");
+        number4Button = new JButton("4");
+        number4Button.addActionListener(this);
+        number4Button.setActionCommand("4");
+        number5Button = new JButton("5");
+        number5Button.addActionListener(this);
+        number5Button.setActionCommand("5");
+        number6Button = new JButton("6");
+        number6Button.addActionListener(this);
+        number6Button.setActionCommand("6");
+        number7Button = new JButton("7");
+        number7Button.addActionListener(this);
+        number7Button.setActionCommand("7");
+        number8Button = new JButton("8");
+        number8Button.addActionListener(this);
+        number8Button.setActionCommand("8");
+        number9Button = new JButton("9");
+        number9Button.addActionListener(this);
+        number9Button.setActionCommand("9");
+
+        //operators
+        addButton       = new JButton("+");
+        addButton.addActionListener(this);
+        addButton.setActionCommand("+");
+        subtractButton  = new JButton("-");
+        subtractButton.addActionListener(this);
+        subtractButton.setActionCommand("-");
+        multiplyButton  = new JButton("*");
+        multiplyButton.addActionListener(this);
+        multiplyButton.setActionCommand("*");
+        divideButton    = new JButton("/");
+        divideButton.addActionListener(this);
+        divideButton.setActionCommand("/");
+        clearButton     = new JButton("C");
+        clearButton.addActionListener(this);
+        clearButton.setActionCommand("C");
+        //dotButton       = new JButton(".");
+        //dotButton.addActionListener(this);
+        //dotButton.setActionCommand(".");
+        equalsButton    = new JButton("=");
+        equalsButton.addActionListener(this);
+        equalsButton.setActionCommand("=");
+
+        //numpad-Panel
+        numpadPanel = new JPanel();
+        numpadPanel.setLayout(new GridLayout(4, 4));
+
+        numpadPanel.add(number7Button); numpadPanel.add(number8Button); numpadPanel.add(number9Button); numpadPanel.add(divideButton);
+        numpadPanel.add(number4Button); numpadPanel.add(number5Button); numpadPanel.add(number6Button); numpadPanel.add(multiplyButton);
+        numpadPanel.add(number1Button); numpadPanel.add(number2Button); numpadPanel.add(number3Button); numpadPanel.add(subtractButton);
+        numpadPanel.add(clearButton);   numpadPanel.add(number0Button); numpadPanel.add(equalsButton);  numpadPanel.add(addButton);
+        //numpadPanel.add(dotButton);
+
+
+        //output-Box
+        currentNumbersField = new JTextField(30);
+        currentNumbersField.setEditable(false);
+        sumField = new JTextField(30);
+        sumField.setEditable(false);
+
+        outputBoxleft = new Box(BoxLayout.Y_AXIS);
+        outputBoxleft.add(Box.createRigidArea(new Dimension(1, 16)));
+        outputBoxleft.add(currentNumbersField);
+        outputBoxleft.add(Box.createRigidArea(new Dimension(1, 16)));
+
+        outputBoxright = new Box(BoxLayout.Y_AXIS);
+        outputBoxright.add(new JLabel("Sum:"));
+        outputBoxright.add(sumField);
+        outputBoxleft.add(Box.createRigidArea(new Dimension(1, 16)));
+
+        outputBox = new Box(BoxLayout.X_AXIS);
+        outputBox.add(outputBoxleft);
+        outputBox.add(Box.createRigidArea(new Dimension(10, 1)));
+        outputBox.add(outputBoxright);
+
+        //Frame
+        add(outputBox, BorderLayout.NORTH);
+        add(numpadPanel, BorderLayout.CENTER);
+
+    }
+
+    //Mthods
+    public void clear_method(){
+        inputString1 = "";
+        inputString2 = "";
+        operatorString = "";
+        sumString = "";
+
+        changeinputString = false;
+        inputtingcurrentNumbers = true;
+    }
+
+    public void equals_method(){
+        inputtingcurrentNumbers = false;
+        num1 = Double.parseDouble(inputString1);
+        num2 = Double.parseDouble(inputString2);
+
+        if(operatorString.equals("/")){
+            sum = num1/num2;
+        }
+        if(operatorString.equals("*")){
+            sum = num1*num2;
+        }
+        if(operatorString.equals("-")){
+            sum = num1-num2;
+        }
+        if(operatorString.equals("+")){
+            sum = num1+num2;
+        }
+
+        sumString = sum + "";
+    }
+
+    public void dooperatorString(ActionEvent e){
+        
+        if(e.getActionCommand().equals("/")){
+            changeinputString = true;
+            operatorString = "/";
+        }
+        if(e.getActionCommand().equals("*")){
+            changeinputString = true;
+            operatorString = "*";
+        }
+        if(e.getActionCommand().equals("-")){
+            changeinputString = true;
+            operatorString = "-";
+        }
+        if(e.getActionCommand().equals("+")){
+            changeinputString = true;
+            operatorString = "+";
+        }
+    }
+
+    public void doinputString1(ActionEvent e){
+        
+        if(e.getActionCommand().equals("0")){
+            inputString1 += "0";
+        }
+        if(e.getActionCommand().equals("1")){
+            inputString1 += "1";
+        }
+        if(e.getActionCommand().equals("2")){
+            inputString1 += "2";
+        }
+        if(e.getActionCommand().equals("3")){
+            inputString1 += "3";
+        }
+        if(e.getActionCommand().equals("4")){
+            inputString1 += "4";
+        }
+        if(e.getActionCommand().equals("5")){
+            inputString1 += "5";
+        }
+        if(e.getActionCommand().equals("6")){
+            inputString1 += "6";
+        }
+        if(e.getActionCommand().equals("7")){
+            inputString1 += "7";
+        }
+        if(e.getActionCommand().equals("8")){
+            inputString1 += "8";
+        }
+        if(e.getActionCommand().equals("9")){
+            inputString1 += "9";
+        }
+    }
+
+    public void doinputString2(ActionEvent e){
+        if(e.getActionCommand().equals("0")){
+            inputString2 += "0";
+        }
+        if(e.getActionCommand().equals("1")){
+            inputString2 += "1";
+        }
+        if(e.getActionCommand().equals("2")){
+            inputString2 += "2";
+        }
+        if(e.getActionCommand().equals("3")){
+            inputString2 += "3";
+        }
+        if(e.getActionCommand().equals("4")){
+            inputString2 += "4";
+        }
+        if(e.getActionCommand().equals("5")){
+            inputString2 += "5";
+        }
+        if(e.getActionCommand().equals("6")){
+            inputString2 += "6";
+        }
+        if(e.getActionCommand().equals("7")){
+            inputString2 += "7";
+        }
+        if(e.getActionCommand().equals("8")){
+            inputString2 += "8";
+        }
+        if(e.getActionCommand().equals("9")){
+            inputString2 += "9";
+        }
+    }
+    
+    //Programm ablauf
+    public void actionPerformed(ActionEvent e) {
+        
+        //Clear
+        if(e.getActionCommand().equals("C")){
+            clear_method();
+        }
+
+        //equals
+        if(!inputString1.isBlank() && !inputString2.isBlank() && !operatorString.isBlank()){
+            if(e.getActionCommand().equals("=")){
+                equals_method();   
+            }
+        }
+
+        if(inputtingcurrentNumbers = true){
+            //operator-Strings
+            dooperatorString(e);
+    
+            //input-Strings
+            if(changeinputString == false){
+               doinputString1(e);
+            }
+            else if(changeinputString == true){
+                doinputString2(e);
+            }
+        }
+
+        currentNumbersField.setText( inputString1 + " " + operatorString + " " + inputString2);
+        sumField.setText(sumString);
+        repaint();
+    }
+
+    public static void main(String[] args){
+        Taschenrechner App = new Taschenrechner();
+        App.setSize(500, 500);
+        App.setVisible(true);
+    }
+}
